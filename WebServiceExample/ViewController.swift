2eaa334d39ed7c2b4182b0460a8cc1b1cc3b9d7f
tim-peterson <petersontimr@gmail.com>
2014-10-30 18:51:00 -0400

    fb app

--- WebServiceExample/ViewController.swift
+++ WebServiceExample/ViewController.swift
@@ -10,8 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.myNameLabel.alpha = 0.0 //hide until we get the data
+        
+        let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+        
+        activityIndicatorView.center = self.view.center
+        self.view.addSubview(activityIndicatorView)
+        activityIndicatorView.startAnimating()
+        
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "http:/graph.facebook.com/petersontimr",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+                
+                if let myName = responseObject.valueForKey("name") as? String {
+                    self.myNameLabel.text = myName;
+                    
+                    activityIndicatorView.stopAnimating()
+                    
+                    UIView.animateWithDuration(1.0, animations: {
+                        self.myNameLabel.alpha = 1.0
+                        }, completion: {
+                            (value: Bool) in
+                        println("Animation complete")
+                    })
+                }
+                
+                
+                
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ bridge.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  WebServiceExample
+//
+//  Created by Tim Peterson on 10/30/14.
+//  Copyright (c) 2014 Tim Peterson. All rights reserved.
+//
+
+#import "AFNetworking/AFNetworking.h"
+
+
+#ifndef WebServiceExample_Header_h
+#define WebServiceExample_Header_h
+
+
+#endif
