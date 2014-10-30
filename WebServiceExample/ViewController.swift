b797804bd8f5f722f843451e342905150ce69fc6
tim-peterson <petersontimr@gmail.com>
2014-10-30 18:26:34 -0400

    Initial Commit

---
+++ WebServiceExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebServiceExample
+//
+//  Created by Tim Peterson on 10/30/14.
+//  Copyright (c) 2014 Tim Peterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
